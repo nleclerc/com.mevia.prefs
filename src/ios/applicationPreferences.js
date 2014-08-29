e9de43ddef88e9ce65b3c889cc5e7f8294f4e4a8
jackTheRipper <baudin.jean@gmail.com>
2014-08-29 17:35:11 +0200

    Fix
    Fix cordova.exec instead of exec [undefined]

--- src/ios/applicationPreferences.js
+++ src/ios/applicationPreferences.js
@@ -1,12 +1,12 @@
 function ApplicationPreferences() { };
 ApplicationPreferences.prototype.get = function(key, successFn, errorFn) {
-	exec(successFn, errorFn, 'applicationPreferences', 'getSetting', [key]);
+	cordova.exec(successFn, errorFn, 'applicationPreferences', 'getSetting', [key]);
 };
 ApplicationPreferences.prototype.set = function(key,value, successFn, errorFn) {
-	exec(successFn, errorFn, 'applicationPreferences', 'setSetting', [key,value]);
+	cordova.exec(successFn, errorFn, 'applicationPreferences', 'setSetting', [key,value]);
 };
 ApplicationPreferences.prototype.clear = function(key, successFn, errorFn) {
-    exec(successFn, errorFn, 'applicationPreferences', 'clearSetting', [key]);
+    cordova.exec(successFn, errorFn, 'applicationPreferences', 'clearSetting', [key]);
 };
 ApplicationPreferences.install = function () {
   window.applicationPreferences = new ApplicationPreferences();
