e6387e692f172949a6f70b5541522e80c1f4a7e8
jackTheRipper <baudin.jean@gmail.com>
2014-09-10 18:29:17 +0200

    Hot fix Android
    Hot fix for for Android

--- src/android/applicationPreferences.java
+++ src/android/applicationPreferences.java
@@ -1,13 +1,10 @@
-package com.mevia.prefs;
+package com.simonmacdonald.prefs;
 
 import java.util.Iterator;
 import java.util.Map;
 //jbaudin : maj Cordova 3.x
 import org.apache.cordova.*;
-import org.apache.cordova.api.*;
-//import org.apache.cordova.api.CallbackContext;
-//import org.apache.cordova.api.CordovaPlugin;
-//import org.apache.cordova.api.PluginResult;
+//import org.apache.cordova.api.*;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -20,40 +17,61 @@ import android.preference.PreferenceManager;
 import android.util.Log;
 
 public class applicationPreferences extends CordovaPlugin {
-
+    
     private static final String LOG_TAG = "AppPrefs";
     private static final int NO_PROPERTY = 0;
     private static final int NO_PREFERENCE_ACTIVITY = 1;
-	
+    
     private static final int SHOW_PREFERENCEACTIVITY_INTENT = 1;
     private CallbackContext auxCtx;
     
     @Override
-    public boolean execute(String action, CordovaArgs args, CallbackContext callbackContext) throw JSONException {
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        PluginResult.Status status = PluginResult.Status.OK;
+        String result = "";
+        
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this.cordova.getActivity());
-
+        
         try {
             if (action.equals("get")) {
-                this.getPreference(args, callbackContext);
+                String key = args.getString(0);
+                if (sharedPrefs.contains(key)) {
+                    Object obj = sharedPrefs.getAll().get(key);
+                    callbackContext.sendPluginResult(new PluginResult(status, obj.toString()));
+                } else {
+                    callbackContext.sendPluginResult(createErrorObj(NO_PROPERTY, "No such property called " + key));
+                }
             } else if (action.equals("set")) {
-                this.setPreference(args, callbackContext);
+                String key = args.getString(0);
+                String value = args.getString(1);
+                Editor editor = sharedPrefs.edit();
+                if ("true".equals(value.toLowerCase()) || "false".equals(value.toLowerCase())) {
+                    editor.putBoolean(key, Boolean.parseBoolean(value));
+                } else {
+                    editor.putString(key, value);
+                }
+                callbackContext.sendPluginResult(new PluginResult(status, editor.commit()));
             } else if (action.equals("load")) {
-				this.load(callbackContext);
-			} else if (action.equals("show")) {
-            	auxCtx = callbackContext;
+                
+                this.load(callbackContext);
+                
+            } else if (action.equals("show")) {
+                auxCtx = callbackContext;
                 String activityName = args.getString(0);
-				
-				this.show(activityName, callbackContext);
-                        
+                
+                this.show(activityName, callbackContext);
+                
             } else if (action.equals("clear")) {
-            	this.clearPreferences(callbackContext);
+                Editor editor = sharedPrefs.edit();
+                editor.clear();
+                callbackContext.sendPluginResult(new PluginResult(status, editor.commit()));
             } else if (action.equals("remove")) {
-            	String key = args.getString(0);            	
-            	if (sharedPrefs.contains(key)) {
-            		Editor editor = sharedPrefs.edit();
-                	editor.remove(key);
-                	editor.commit();
-                	callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, editor.commit()));
+                String key = args.getString(0);
+                if (sharedPrefs.contains(key)) {
+                    Editor editor = sharedPrefs.edit();
+                    editor.remove(key);
+                    editor.commit();
+                    callbackContext.sendPluginResult(new PluginResult(status, editor.commit()));
                 } else {
                     callbackContext.sendPluginResult(createErrorObj(NO_PROPERTY, "No such property called " + key));
                 }
@@ -62,97 +80,58 @@ public class applicationPreferences extends CordovaPlugin {
         } catch (JSONException e) {
             status = PluginResult.Status.JSON_EXCEPTION;
         }
+        callbackContext.sendPluginResult(new PluginResult(status, result));
         return false;
     }
-
-    private void getPreference(CordovaArgs args, CallbackContext callbackContext) {
-        String key = args.getString(0);
-        if (sharedPrefs.contains(key)) {
-            Object obj = sharedPrefs.getAll().get(key);
-            callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, obj.toString()));
-        } else {
-            callbackContext.error("No such property called " + key);
-        }
-    }
     
-    private void setPreference(CordovaArgs args, CallbackContext callbackContext) {
-        try {
-            String key = args.getString(0);
-            String value = args.getString(1);
-            Editor editor = sharedPrefs.edit();
-            if ("true".equals(value.toLowerCase()) || "false".equals(value.toLowerCase())) {
-                editor.putBoolean(key, Boolean.parseBoolean(value));
-            } else {
-                editor.putString(key, value);
+    private void load(CallbackContext callbackContext) throws JSONException {
+        
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this.cordova.getActivity());
+        Map map = sharedPrefs.getAll();
+        Iterator entries = map.entrySet().iterator();
+        JSONObject response = new JSONObject();
+        while(entries.hasNext()) {
+            Map.Entry entry = (Map.Entry) entries.next();
+            String key = (String)entry.getKey();
+            String value = (String)entry.getValue();
+            
+            try {
+                response.put(key, value);
+            } catch (JSONException e) {
+                continue;
             }
-            callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, editor.commit()));
-        } catch (JSONException e) {
-            callbackContext.error("Some arguments are missing");
         }
+        callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, response));
     }
     
-    private void clearPreferences(CallbackContext callbackContext) {
-        Editor editor = sharedPrefs.edit();
-        editor.clear();
-        callbackContext.sendPluginResult(new PluginResult(status, editor.commit()));
-    }
-    
-    private void removePreferences(CordovaArgs args, CallbackContext callbackContext) {
-        String key = args.getString(0);
-        if (sharedPrefs.contains(key)) {
-            Editor editor = sharedPrefs.edit();
-            editor.remove(key);
-            editor.commit();
-            callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, editor.commit()));
-        } else {
-            callbackContext.sendPluginResult(createErrorObj(NO_PROPERTY, "No such property called " + key));
+    private void show(String prefActivity, CallbackContext callbackContext) throws JSONException {
+        
+        String activityClass = prefActivity;
+        auxCtx = callbackContext;
+        
+        try {
+            Class a = Class.forName(activityClass);
+            
+            Intent intent = new Intent(this.cordova.getActivity(), a);
+            intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+            
+            this.cordova.startActivityForResult(this, intent, SHOW_PREFERENCEACTIVITY_INTENT);
+            
+        } catch( ClassNotFoundException e ) {
+            callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR, "No preferences activity called " + prefActivity));
         }
+        
     }
     
-	private void load(CallbackContext callbackContext) {
-
-		SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this.cordova.getActivity());
-		Map map = sharedPrefs.getAll();
-		Iterator entries = map.entrySet().iterator();
-		JSONObject response = new JSONObject();
-		while(entries.hasNext()) {
-			Map.Entry entry = (Map.Entry) entries.next();
-			String key = (String)entry.getKey();
-		    String value = (String)entry.getValue();
-		    
-		    try {
-				response.put(key, value);
-			} catch (JSONException e) {
-				continue;
-			}
-		}
-	 	callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, response));	
-	}
-	
-	private void show(String prefActivity, CallbackContext callbackContext) {
-    	
-		String activityClass = prefActivity;
-		auxCtx = callbackContext;
-		
-		try {
-    		Class a = Class.forName(activityClass);
-    		 
-    		Intent intent = new Intent(this.cordova.getActivity(), a);
-    		intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-    		   			
-    		this.cordova.startActivityForResult(this, intent, SHOW_PREFERENCEACTIVITY_INTENT);
-    		 
-    	} catch( ClassNotFoundException e ) {
-    		callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR, "No preferences activity called " + prefActivity));			
-    	}	  
-
-    }
-
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-    	
-	    if(requestCode == SHOW_PREFERENCEACTIVITY_INTENT) {
-	    	this.load(auxCtx);		
-		}
+        
+        if(requestCode == SHOW_PREFERENCEACTIVITY_INTENT) {
+            try {
+                this.load(auxCtx);
+            }	catch (JSONException e) {
+                System.console().printf("caca");
+            }
+        }
     }
     
     private PluginResult createErrorObj(int code, String message) throws JSONException {
@@ -161,5 +140,5 @@ public class applicationPreferences extends CordovaPlugin {
         errorObj.put("message", message);
         return new PluginResult(PluginResult.Status.ERROR, errorObj);
     }
-
+    
 }
